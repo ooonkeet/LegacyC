c6ad75768031077d24f5fc78560b5656a8773ff9
Ankit <ankitspare981@gmail.com>
2026-01-03 15:04:52 +0530

    find out LCM of two numbers

--- maths/lcm.c
+++ maths/lcm.c
@@ -5,11 +5,11 @@ int main(){
     scanf("%d",&a);
     printf("Enter number 2:- ");
     scanf("%d",&b);
-    maximum=(a>b)?a:b;
 /*
 
 ------------ALTERNATE INFINTE LOOP VERSION----------------------
 
+    maximum=(a>b)?a:b;
     while(1){
         if(maximum%a==0 && maximum%b==0){
             printf("LCM of %d and %d is = %d.",a,b,maximum);
