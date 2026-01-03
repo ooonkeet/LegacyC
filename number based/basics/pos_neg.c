444db6b419ecd74db517a1e8efcd83bddd8a0690
Ankit <ankitspare981@gmail.com>
2026-01-03 14:27:40 +0530

    identify A number is positive or negative

---
+++ number based/basics/pos_neg.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number:- ");
+    scanf("%d",&n);
+    if(n<0)
+        printf("%d is negative.",n);
+    else if(n>0)
+        printf("%d is positive.",n);
+    else
+        printf("%d is zero.",n);
+    return 0;
+}
