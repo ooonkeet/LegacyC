eb56e233c20cbc71fb5a7b23b9a9638584466bc9
Ankit <ankitspare981@gmail.com>
2026-01-13 03:32:41 +0530

    basic matrix operation - difference and sum of two matrices

---
+++ matrix/basic operations/diff.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+void input(int row,int col,int mat[row][col]){
+    printf("Enter value for the matrix.\n");
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            printf("Enter element (%d,%d):- ",i+1,j+1);
+            scanf("%d",&mat[i][j]);
+        }
+    }
+}
+void display(int row,int col,int mat[row][col]){
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            printf("%d\t",mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main(){
+    int row,col;
+    printf("Enter number of rows:- ");
+    scanf("%d",&row);
+    printf("Enter number of columns:- ");
+    scanf("%d",&col);
+    printf("\n\n");
+    int a[row][col],b[row][col],c[row][col];
+    input(row,col,a);
+    printf("\n-------------------------------------\n");
+    input(row,col,b);
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            c[i][j]=abs(a[i][j]-b[i][j]);
+        }
+    }
+    printf("---------------------------------------\n\n");
+    printf("First Matrix is\n");
+    display(row,col,a);
+    printf("\n");
+    printf("Second Matrix is\n");
+    display(row,col,b);
+    printf("\n");
+    printf("Absolute Difference of the matrices are\n");
+    display(row,col,c);
+    printf("----------------------------------------");
+    return 0;
+}
