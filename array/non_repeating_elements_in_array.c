b977100bcd1d321cc38eb9e14bb0568993f5fda8
Ankit <ankitspare981@gmail.com>
2026-01-20 03:37:47 +0530

    print and count Non-repeating elements of an array

---
+++ array/non_repeating_elements_in_array.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+int part(int a[],int left,int right){
+    int piv=a[left],i=left+1,j=right,temp;
+    while(i<=j){
+        while(i<=right && a[i]<=piv)
+            i++;
+        while(j>=left && a[j]>piv)
+            j--;
+        if(i<j){
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+        }
+    }
+    temp=a[left];
+    a[left]=a[j];
+    a[j]=temp;
+    return j;
+}
+void sort(int arr[],int l,int r){
+    if(l<r){
+        int p=part(arr,l,r);
+        sort(arr,l,p-1);
+        sort(arr,p+1,r);
+    }
+}
+int main(){
+    int n;
+    printf("Enter size of the array: ");
+    scanf("%d",&n);
+    int arr[n],dupl[n],idx=0;
+    printf("\nEnter elements of array one by one.\n\n");
+    for(int i=0;i<n;i++){
+        printf("Enter element %d:- ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    sort(arr,0,n-1);
+    if(n==1)
+        printf("\n%d non repeating element is present which is = %d.\n",1,arr[0]);
+    else{
+        for(int i=0;i<n;i++){
+        if(i==0)
+            if(arr[i]!=arr[i+1])
+                dupl[idx++]=arr[i];
+        if(i==n-1)
+            if(arr[i]!=arr[i-1])
+                dupl[idx++]=arr[i];
+        if(i<n-1 && i>0)
+            if(arr[i]!=arr[i-1] && arr[i]!=arr[i+1])
+                dupl[idx++]=arr[i];
+        }
+        if(idx==0)
+            printf("\nThey are all repeating elements!\n");
+        else{
+            printf("\nNo of non repeating elements = %d.\n",idx);
+            printf("\nNon repeating elements are:  ");
+            for(int i=0;i<idx;i++)
+                printf("%d  ",dupl[i]);
+        }
+    }
+    return 0;
+}
