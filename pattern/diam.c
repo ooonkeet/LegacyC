52a59c67aaa9e9450ef7bb79e08e9f55fff7eddc
Ankit <ankitspare981@gmail.com>
2026-01-23 00:10:09 +0530

    print Diamond pattern using numbers

---
+++ pattern/diam.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter number of lines:- ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=n-1;j>=i;j--)
+            printf("  ");
+        for(int j=1;j<=i;j++)
+            printf("%d ",j);
+        for(int j=i;j>=2;j--)
+            printf("%d ",j-1);
+        printf("\n");
+    }
+    for(int i=2;i<=n;i++){
+        for(int sp=i;sp>1;sp--)
+            printf("  ");
+        for(int j=i;j<=n;j++)
+            printf("%d ",j);
+        for(int j=n-1;j>=i;j--)
+            printf("%d ",j);
+        printf("\n");
+    }
+    return 0;
+}
